f0a712c47e84e5c2bad215cb3b247724301a2d25
bencheee <sandro.bencinic@gmail.com>
2021-10-15 13:48:47 +0000

    Add show/hide functionality to flash messages

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,11 @@
 $(".navbar__toggle").click(function () {
     $(".nav__mobile").slideToggle();
 })
+
+// Shows flash message
+$(".flash").slideToggle();
+
+// Hides flash message
+$("#flash-close").click(function(){
+    $(".flash").slideUp();
+})
