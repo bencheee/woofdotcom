be9d11ba5f55519f69c1065948c8380b6d6d64a3
bencheee <sandro.bencinic@gmail.com>
2021-10-16 10:32:14 +0000

    Add password toggle functionality to user_login

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,8 @@
+// ####################
+//        EVENTS
+// ####################
+
+
 // Navbar toggle
 $(".navbar__toggle").click(function () {
     $(".nav__mobile").slideToggle();
@@ -10,3 +15,30 @@ $(".flash").slideToggle();
 $("#flash-close").click(function(){
     $(".flash").slideUp();
 })
+
+// Toggles password visibility
+$("#pass_icon").click(passToggle);
+
+
+// ####################
+//      FUNCTIONS
+// ####################
+
+
+// Toggles password visibility
+function passToggle() {
+    let pass = $("#password")[0]
+    if (pass.type === "password") {
+        pass.type = "text";
+        $("#password").css({"font-weight": "100", "letter-spacing": "0"});
+        $("#pass_icon").removeClass("fa-eye");
+        $("#pass_icon").addClass("fa-eye-slash");
+        $("#pass_toggle").text("Hide password");
+    } else {
+        pass.type = "password";
+        $("#password").css({"font-weight": "900", "letter-spacing": "3px"});
+        $("#pass_icon").removeClass("fa-eye-slash");
+        $("#pass_icon").addClass("fa-eye");
+        $("#pass_toggle").text("Show password");
+    }
+}
