46bc7582e6b428511d7ceb5a20d560710c666862
bencheee <sandro.bencinic@gmail.com>
2021-10-31 11:10:26 +0000

    Add logo to desktop navbar

--- static/js/script.js
+++ static/js/script.js
@@ -186,13 +186,11 @@ $(window).scroll(function() {
 $(".nav__username").mouseover(
     function () {
         $(".nav__dropdown").slideDown();
-        $(".nav__dropdown").css("display", "flex")
     })
 
 $(".desk-nav").mouseleave(
     function () {
         $(".nav__dropdown").slideUp();
-        // $(".nav__dropdown").css("display", "none")
     })
 
 
